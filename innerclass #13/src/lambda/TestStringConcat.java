5f07ae6beabcb1011e4ed92c3290dc01f2793cef
SeoYounSeok <jvn3339@naver.com>
2019-01-17 03:38:51 +0900

    lambda
    람다까지 두개 비교

---
+++ innerclass #13/src/lambda/TestStringConcat.java
@@ -0,0 +1,31 @@
+package lambda;
+
+public class TestStringConcat {
+
+	public static void main(String[] args) {
+
+		String s1 = "Hello";
+		String s2 = "World";
+		StringComcatlmpl concat1 = new StringComcatlmpl();
+		concat1.makeString(s1, s2);
+		
+		int i = 100;
+		StringConcat concat2 = (s, v)->System.out.println(s + "," + v );
+		//System.out.println(i);
+		concat2.makeString(s1, s2);
+		i = 300;
+		
+		StringConcat concat3 = new StringConcat() {
+			
+			@Override
+			public void makeString(String s1, String s2) {
+				
+				System.out.println( s1 + "," + s2 );
+			}
+		};
+		
+		concat3.makeString(s1, s2);
+		
+	}
+
+}
